bba9d4951bca7f9af62f0d698f8019183f05c549
jfkeci <ja.lip132@gmail.com>
2021-08-05 14:57:51 +0200

    gui app

---
+++ file-upload/main.py
@@ -0,0 +1,13 @@
+import tornado.web
+import tornado.ioloop
+
+
+if(__name__ == "__main__"):
+    app = tornado.web.Application([
+        ("/img/(.*)", tornado.web.StaticFileHandler, {"path": "img"})
+    ])
+
+    app.listen(8080)
+    app.print("listening on port 8080")
+
+    tornado.ioloop.IOLoop.instance().start()
